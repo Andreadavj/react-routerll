cb8d7b692dd78a02d7c2ec963a897a1d1a230427
Doris Andrea Valverde Jara <andrea.avj03@gmail.com>
2024-11-04 21:12:50 -0300

    navbar

--- react-ll/src/components/Navbar.jsx
+++ react-ll/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { CartContext } from '../contexts/cartContext';
 import { useUser } from '../contexts/UserContext';
 
@@ -7,27 +7,29 @@ const Navbar = () => {
   const { total } = useContext(CartContext);
   const { token, logout } = useUser();
 
+  const setActiveClass = ({ isActive }) => (isActive ? "active" : undefined);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Pizzería Mamma Mia!</Link>
+        <NavLink className="navbar-brand" to="/">Pizzería Mamma Mia!</NavLink>
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="navbar-nav">
-            <Link className="nav-link btn btn-outline-success me-2" to="/">Home 🍕</Link>
+            <NavLink className={`nav-link btn btn-outline-success me-2 ${setActiveClass}`} to="/">Home 🍕</NavLink>
             {token ? (
               <>
-                <Link className="nav-link btn btn-outline-success me-2" to="/profile">Profile 👨‍💻</Link>
+                <NavLink className={`nav-link btn btn-outline-success me-2 ${setActiveClass}`} to="/profile">Profile 👨‍💻</NavLink>
                 <button onClick={logout} className="nav-link btn btn-outline-success me-2">Logout 🔐</button>
               </>
             ) : (
               <>
-                <Link className="nav-link btn btn-outline-success me-2" to="/login">Login 🚪</Link>
-                <Link className="nav-link btn btn-outline-success me-2" to="/register">Register 🔐</Link>
+                <NavLink className={`nav-link btn btn-outline-success me-2 ${setActiveClass}`} to="/login">Login 🚪</NavLink>
+                <NavLink className={`nav-link btn btn-outline-success me-2 ${setActiveClass}`} to="/register">Register 🔐</NavLink>
               </>
             )}
           </div>
           <div className="navbar-nav ms-auto">
-            <Link className="nav-link btn btn-outline-success" to="/cart">🛒 Total: ${total.toFixed(2)}</Link>
+            <NavLink className={`nav-link btn btn-outline-success`} to="/cart">🛒 Total: ${total.toFixed(2)}</NavLink>
           </div>
         </div>
       </div>
